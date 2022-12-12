5371f29da609bd6f7607762c6e91a2fca35a73e1
lucianocecere07 <cecereluciano97@gmail.com>
2022-12-12 17:51:09 +0100

    escluso prima riga

--- ListaIndirizzi/Indirizzo.cs
+++ ListaIndirizzi/Indirizzo.cs
@@ -56,68 +56,11 @@ namespace ListaIndirizzi
         {
             return cap;
         }
-        /* PROVA
-        //setter
-        public void SetNome(string nome)
-        {
-            if (nome == "" || nome == " ")
-            {
-                throw new ArgumentException("nome", "campo del nome errato");
-            }
-            this.nome = nome;  
-        }
-        public void SetCognome(string cognome)
-        {
-            if (cognome == "" || cognome == " ")
-            {
-                throw new ArgumentException("cognome", "campo del cognome errato");
-            }
-            this.cognome = cognome;
-        }
-        public void SetVia(string via)
-        {
-            if (via == "" || via == " ")
-            {
-                throw new ArgumentException("via", "campo della via errato");
-            }
-            this.via = via;
-        }
-        public void SetCitta(string citta)
-        {
-            if (citta == "" || citta == " ")
-            {
-                throw new ArgumentException("citta", "campo della città errato");
-            }
-            this.citta = citta;
-        }
-        public void SetProvincia(string provincia)
-        {
-            if (provincia == "" || provincia == " ")
-            {
-                throw new ArgumentException("provincia", "campo della provincia errato");
-            }
-            this.provincia = provincia;
-        }
-        public void SetCap(string cap)
-        {
-            if (cap == "" || cap == " ")
-            {
-                throw new ArgumentException("cap", "campo del cap errato");
-            }
-            this.cap = cap;
-        }
-        */
+       
         //metodi
         public override string ToString()
         {
             return "---" + "\nnome: " + nome + "\ncognome: " + cognome + "\nvia: " + via + "\ncittà: " + citta + "\nprovincia: " + provincia + "\ncap: " + cap + "\n---";
         }
-       
-        /* PROVA
-        public override string ToString()
-        {
-            return "" + nome + "\n" + cognome + "\n" + via + "\n" + citta + "\n" + provincia + "\n" + cap + "\n------";
-        }
-       */
     }
 }
--- ListaIndirizzi/Program.cs
+++ ListaIndirizzi/Program.cs
@@ -8,21 +8,27 @@ List<Indirizzo> listaIndirizzi = new List<Indirizzo>();
 try
 {
     StreamReader file = File.OpenText("C:\\File Luciano\\Studi\\dotnet generation\\esercizi GIT\\ListaIndirizzi\\ListaIndirizzi\\file\\addresses.csv");
+    
+    file.ReadLine();
+    
     while (!file.EndOfStream)
     {
         string line = file.ReadLine();
 
         string[] infoIndirizzo = line.Split(",");
+        if (infoIndirizzo.Length == 6)
+        {
+            string nome = infoIndirizzo[0];
+            string cognome = infoIndirizzo[1];
+            string via = infoIndirizzo[2];
+            string citta = infoIndirizzo[3];
+            string provincia = infoIndirizzo[4];
+            string cap = infoIndirizzo[5];
 
-        string nome = infoIndirizzo[0];
-        string cognome = infoIndirizzo[1];
-        string via = infoIndirizzo[2];
-        string citta = infoIndirizzo[3];
-        string provincia = infoIndirizzo[4];
-        string cap = infoIndirizzo[5];
+            Indirizzo indirizzo = new Indirizzo(nome, cognome, via, citta, provincia, cap);
+            listaIndirizzi.Add(indirizzo);
 
-        Indirizzo indirizzo = new Indirizzo(nome, cognome, via, citta, provincia, cap);
-        listaIndirizzi.Add(indirizzo);
+        }
     }
     file.Close();
 }
@@ -35,11 +41,3 @@ foreach (Indirizzo indirizzo in listaIndirizzi)
 {
     Console.WriteLine(indirizzo);
 }
-
-
-//------//
-
-/*  PROVA
-//"C:\\File Luciano\\Studi\\dotnet generation\\esercizi GIT\\ListaIndirizzi\\ListaIndirizzi\\file\\addresses.csv"
-//"C:\\File Luciano\\Studi\\dotnet generation\\esercizi-prove non su github\\prove-VS2022\\documento.txt"
-*/
